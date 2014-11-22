cceedc92b91198b145e4e35121210c5771f5752f
tragicjun <zhangjun2915@163.com>
2014-11-22 17:17:24 +0800

    update

--- app.go
+++ app.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	//"fmt"
+	"fmt"
 	"log"
 	"os"
-	//"os/exec"
+	"os/exec"
+
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-docopt"
 )
 
@@ -19,27 +21,18 @@ Examples:
 	$ flynn create dsf
 	Created dsf
 `)
-	register("update", runUpdate, `
-usage: flynn update <name>
-
-Update an application in Flynn.
-
-Examples:
-
-	$ flynn update dsf
-	Updating dsf
-`)
 	register("deploy", runDeploy, `
-usage: flynn deploy [-s <url>]
+usage: flynn deploy -a <appName> [-s <url>]
 
 Options:
+	-a set app name
 	-s, --svn-url <url>  set the svn url of your code
 
 Deploy an application in Flynn.
 
 Examples:
 
-	$ flynn deploy -s http://svnURL
+	$ flynn deploy -a dsf -s http://svnURL
 	Exporing svn code
 	Compiling code
 	Deployed
@@ -56,23 +49,18 @@ func runCreate(args *docopt.Args) error {
 	return nil
 }
 
-func runUpdate(args *docopt.Args) error {
-	var appName = args.String["<name>"]
-
-	os.Setenv("JFLYNN_APP", appName)
-	log.Printf("Start updating %s", os.Getenv("JFLYNN_APP"))
-	return nil
-}
-
 func runDeploy(args *docopt.Args) error {
+	var appName = args.String["-a"]
 	var svn = args.String["--svn-url"]
 	log.Printf("Exporting %s...", svn)
-	var cmd = "docker run -it -v /tmp:/tmp -a stdout tegdsf/centos svn export " + svn + " /tmp/slug"
+	d, _ := exec.Command("docker", "run", "-it", "centos", "echo haha").Output()
+	fmt.Println(string(d))
+	var cmd = "docker run -it -v /tmp:/tmp -a stdout tegdsf/centos svn export " + svn + " /tmp/" + appName
 	log.Println(cmd)
-	cmd = "tar cvf slug.tar --directory=/tmp/slug ."
+	cmd = "tar cvf /tmp/" + appName + ".tar --directory=/tmp/" + appName + " ."
 	cmd = "cat slug.tar | docker run -i -v /tmp/buildpacks:/tmp/buildpacks -e HTTP_SERVER_URL=http://192.168.59.103:8080 -a stdin flynn/slugbuilder - > /tmp/slug.tgz"
 	log.Println("Compiling code...")
 
-	log.Printf("Created release for app %s", os.Getenv("JFLYNN_APP"))
+	log.Printf("Created release for app %s", appName)
 	return nil
 }
